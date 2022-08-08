ed0493b7716e116026d4e7a8b4bcd48914e64b5f
Martin Ivan Rios <irios@phorus.group>
2022-08-09 01:13:16 +0300

    Add TargetClasses.kt containing the class wrapper TargetClass

--- src/test/kotlin/group/phorus/mapper/helper/TargetClassesTest.kt
+++ src/test/kotlin/group/phorus/mapper/helper/TargetClassesTest.kt
@@ -1,15 +1,18 @@
 package group.phorus.mapper.helper
 
+import group.phorus.mapper.MapFrom
+import group.phorus.mapper.enums.MappingFallback
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
 import kotlin.reflect.typeOf
 import kotlin.test.assertEquals
 
-internal class OriginClassesTest {
+internal class TargetClassesTest {
 
     @Test
-    fun `wrap object in OriginEntity`() {
+    fun `wrap class in TargetClass`() {
         data class Pet(
+            @field:MapFrom(["../name"], MappingFallback.NULL)
             val name: String,
             val age: Int,
             val breed: String?
@@ -21,42 +24,35 @@ internal class OriginClassesTest {
             val pet: Pet,
         )
 
-        val person = Person(
-            name = "testPersonName",
-            age = 22,
-            pet = Pet(
-                name = "testPetName",
-                age = 3,
-                breed = null
-            )
-        )
+        val result = targetClass<Person>()
 
-        val result = OriginEntity(person)
+        println(result)
 
         // Person asserts
         assertEquals(3, result.properties.size)
 
         assertEquals(typeOf<String>(), result.properties.single { it.name == "name" }.type)
-        assertEquals("testPersonName", result.properties.single { it.name == "name" }.value)
+        assertNull(result.properties.single { it.name == "name" }.mapFrom)
 
         assertEquals(typeOf<Int>(), result.properties.single { it.name == "age" }.type)
-        assertEquals(22, result.properties.single { it.name == "age" }.value)
+        assertNull(result.properties.single { it.name == "age" }.mapFrom)
 
         // Pet asserts
         assertEquals(typeOf<Pet>(), result.properties.single { it.name == "pet" }.type)
-        assertEquals(person.pet, result.properties.single { it.name == "pet" }.value)
+        assertNull(result.properties.single { it.name == "pet" }.mapFrom)
 
         val petNode = result.properties.single { it.name == "pet" }
 
         assertEquals(3, petNode.properties.size)
 
         assertEquals(typeOf<String>(), petNode.properties.single { it.name == "name" }.type)
-        assertEquals("testPetName", petNode.properties.single { it.name == "name" }.value)
+        assertEquals("../name", petNode.properties.single { it.name == "name" }.mapFrom?.locations?.first())
+        assertEquals(MappingFallback.NULL, petNode.properties.single { it.name == "name" }.mapFrom?.fallback)
 
         assertEquals(typeOf<Int>(), petNode.properties.single { it.name == "age" }.type)
-        assertEquals(3, petNode.properties.single { it.name == "age" }.value)
+        assertNull(petNode.properties.single { it.name == "age" }.mapFrom)
 
         assertEquals(typeOf<String?>(), petNode.properties.single { it.name == "breed" }.type)
-        assertNull(petNode.properties.single { it.name == "breed" }.value)
+        assertNull(petNode.properties.single { it.name == "breed" }.mapFrom)
     }
 }
