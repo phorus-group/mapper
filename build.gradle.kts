33ccac74e7bd543c63de99331e09fbc073bb8b10
Martin Ivan Rios <irios@phorus.group>
2022-09-10 10:25:30 +0000

    Test the new publish, close and release option in the pipeline by deploying a new patch version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.net.URL
 import java.time.LocalDate
@@ -15,7 +14,7 @@ plugins {
 
 group = "group.phorus"
 description = "Kotlin based mapper with extra funcitonalities."
-version = "1.0.0"
+version = "1.0.1"
 
 java.sourceCompatibility = JavaVersion.VERSION_17
 java.withSourcesJar()
