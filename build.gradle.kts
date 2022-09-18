de8e8301ea6b2b4f9d9752e650b796d3e911b584
Martin Ivan Rios <irios@phorus.group>
2022-09-18 18:11:58 +0000

    Add environment name and URL to the publish job

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 
 group = "group.phorus"
 description = "Kotlin based mapper with extra funcitonalities."
-version = "1.0.3"
+version = "1.0.4"
 
 java.sourceCompatibility = JavaVersion.VERSION_17
 java.withSourcesJar()
