2bb6c712d06ca85b54de458ffba610a7848dfaf2
Martin Rios <irios@phorus.group>
2026-03-23 08:45:33 +0200

    Exclude boilerplate from tet coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,6 +52,18 @@ tasks {
     jacocoTestReport {
         executionData.setFrom(fileTree(project.layout.buildDirectory).include("/jacoco/*.exec"))
 
+        classDirectories.setFrom(
+            sourceSets.main.get().output.classesDirs.map { dir ->
+                fileTree(dir).exclude(
+                    "**/model/**",
+                    "**/dtos/**",
+                    "**/config/**",
+                    "**/repositories/**",
+                    "**/*Application*",
+                )
+            }
+        )
+
         reports {
             xml.required.set(true)
             csv.required.set(true)
@@ -128,21 +140,6 @@ afterEvaluate {
     }
 }
 
-afterEvaluate {
-    tasks.named<JacocoReport>("jacocoTestReport") {
-        classDirectories.setFrom(files(classDirectories.files.map {
-            fileTree(it) {
-                exclude(
-                    "**/model/**",
-                    "**/dtos/**",
-                    "**/config/**",
-                    "**/repositories/**",
-                    "**/*Application*",
-                )
-            }
-        }))
-    }
-}
 
 mavenPublishing {
     coordinates(
